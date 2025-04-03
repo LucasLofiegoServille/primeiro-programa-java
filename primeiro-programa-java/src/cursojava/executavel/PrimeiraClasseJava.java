0bedcff322817f074b47a3a0ed9713cb8548feba
usuario <usuario@131.0.2.66>
2025-04-03 18:00:42 -0300

    Iniciando a próxima aula de get and set

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,27 +1,43 @@
 package cursojava.executavel;
 
+import cursojava.classes.Aluno;
+
 public class PrimeiraClasseJava {
 
+	public static void main (String[] args) {
 	
 	
-	/*Main é um método auto executável em Java*/
-	public static void main(String[] args) {
-			
+	
+	/*Ainda não existe na memória*/
+	Aluno aluno1 = new Aluno();/*Aqui será o João*/
+	aluno1.nome = "João";
+	aluno1.idade = 36;
+	
+	System.out.println("Nome do aluno1 é = " + aluno1.nome);
+	System.out.println("A idade do aluno1 é = " + aluno1.idade);
+	
+	/*Agora temos um objeto real na memória*/
+	
+	
+	/*new Aluno é uma intâncias (Criação de Objeto)*/
+	/*aluno1 aluno2, são as referências para o objeto Aluno*/
+	/*Atributos são as características do objeto*/
+	Aluno aluno2 = new Aluno ();/*Aqui será o Pedro*/
+	
+	Aluno aluno3 = new Aluno ();/*Aqui será o Alex*/
+	
+	Aluno aluno4 = new Aluno("Maria");
+	
+	Aluno alun5 = new Aluno("José", 50);
+	
+	
+	
 
-		/*Char representa um tipo caractere*/
-		char pessoafisica = 'F';
-		
-		char pessoajuridica = 'J';
-		
-		char pessoamasculina = 'P';
-		
-		if(pessoamasculina == 'M')
-			System.out.println("Masculino");
-		else {
-			System.out.println("Feminino");
-			
-			
-		} 
-			
-	}
+ 
+	}		
 }
+
+
+
+
+	
