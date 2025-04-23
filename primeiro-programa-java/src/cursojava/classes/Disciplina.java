c3ddd0c034b87c5fb3dad9ec5af1d1938463879d
usuario <usuario@131.0.2.66>
2025-04-23 15:56:25 -0300

    Atualizando

--- primeiro-programa-java/src/cursojava/classes/Disciplina.java
+++ primeiro-programa-java/src/cursojava/classes/Disciplina.java
@@ -2,4 +2,27 @@ package cursojava.classes;
 
 public class Disciplina {
 
+	
+	private String disciplina;
+	private String nota;
+	public String getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+	public String getNota() {
+		return nota;
+	}
+	public void setNota(String nota) {
+		this.nota = nota;
+	}
+	@Override
+	public String toString() {
+		return "Disciplina [disciplina=" + disciplina + ", nota=" + nota + "]";
+	}
+	
+	
+	
+			
 }
